03b62e8a8c188a573f4d704e02c6dbc642f2686b
mohamedmehdigara <garamehdi57@yahoo.com>
2023-02-15 15:29:55 +0100

    added files

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar.jsx'
 import HomeConnect, { ConnectAcceptList, ConnectRejectList, ConnectBlockList } from './Container/HomeConnect.jsx'
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from 'react';
 
 function App() {
   return (
